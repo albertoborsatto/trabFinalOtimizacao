c0ab1e85ed48bd71b3de104c51030f4dfd6d87d9
quinhas123 <lucasmschirmbeck@gmail.com>
2024-01-27 15:30:40 -0300

    comentarios do professor.

--- lahc.py
+++ lahc.py
@@ -19,26 +19,19 @@ def lista_com_menor_comprimento(lista_de_listas):
 
     return min(lista_de_listas, key=len)
 
-# HB: isso retorna o peso da bin de maior peso não? Isso não é o objetivo
-# do problema, o fitness da solução tem de ser o número de bins usadas
-# (e você quer minimizar esse número). Vocês podem ter tiebreakers em caso
-# de empate, mas o principal número é o número de bins usadas.
 def evaluate_solution(old_solution, new_solution):
     if(len(old_solution) <= len(new_solution)):
         return old_solution
     else:
         return new_solution
 
-# HB: Essa função cria um vizinho aleatório, não explora a vizinhança.  É
-# necessário que para cada solução, vocês vão gerando todos os possíveis
-# vizinhos sistematicamente (de preferência um por vez para não encher a
-# memória), e dai peguem o primeiro que melhora, ou o que melhora mais de todos
-# (é necessário implementar algum critério de desempate, já que grande parte
-# das soluções vai ter o mesmo valor de solução, como considerar melhor as
-# soluções com pouco peso na bin de menor peso). EDIT: eu acho que vocês
-# interpretaram mal o "Escolhe s' em N(s)" do slide, ele é escolher um vizinho
-# por "first improvement" ou por "best improvement" como eu mencionei acima.
 #escolher um cesto aleatório e adicionar um item aleatório a ele (verificando se é uma solução válida, onde a restrição de capacidade é respeitada)
+# HB: isso está bastante melhor do que antes, mas ainda há 2 problemas:
+# (1) a fila/histórico lh que não é uma fila/histórico;
+# (2) em questões de performance, se deveria calcular qual o valor que a nova
+# solução vai ter, e ver se vai aceitar ou não ela antes de criar a mesma,
+# não criar para depois jogar fora (e deixar os tamanhos das bins já
+# pré-calculados também, para não ter de chamar sum toda vez).
 def neighbor(current_solution, bin_capacity, s, lh):
     num_bins = len(current_solution)
 
@@ -59,14 +52,15 @@ def neighbor(current_solution, bin_capacity, s, lh):
                         solution_found = late_acceptance(s, lh, new_solution)
                         return solution_found
 
-    return None  # Retorna None se nenhuma solução melhor for encontrada
+    return current_solution  # Retorna None se nenhuma solução melhor for encontrada
 
+# HB: as observações sobre performances seguem, vocês deveria calcular os
+# valores das soluções quando vocês alteram elas, e não recalcular toda vez.
+# A lista lh pode guardar só valores, não precisa guardar soluções.
 def late_acceptance(s, lh, new_solution):
-    # HB: Performance: As soluções nunca deveriam ser avaliadas a partir
-    # do zero, lembrem-se que na aula eu falei que a principal coisa a ser
-    # avaliada sobre performance é que o valor das soluções deve ser
-    # calculado quando elas são modificadas e de forma incremental.
     best_solution1 = evaluate_solution(s, new_solution)
+    # HB: essa lista não está sendo usada como fila, só se está utilizando
+    # a primeira posição, sempre. Isso é uma variável disfarçada de lista.
     best_solution2 = evaluate_solution(lh[0], new_solution)
 
     if(best_solution1 == new_solution or best_solution2 == new_solution):
@@ -77,49 +71,40 @@ def late_acceptance(s, lh, new_solution):
     if(best_solution3 == s):
         lh[0] = s
 
-    return [lista_com_menor_comprimento(lh), s, lh[0]]
+    return lista_com_menor_comprimento(lh), s, lh[0]
 
 
-    # HB: Esse código abaixo é completo nonsense. O LAHC não usa critério de
-    # aleatoriedade para aceitação. O único critério do LAHC é se a solução é
-    # melhor que a solução há h iterações atrás (onde h é o único parâmetro da
-    # meta-heurística). Não há uma lista de valores de solução de tamanho h
-    # sendo mantida/passada onde verificar essa informação. Essa lista é a
-    # única estrutura de dados obrigatória do LAHC.
 def bin_packing_lahc(items, bin_capacity):
-    current_solution = [[2],[4],[5],[5]]
+    current_solution = [[]]
 
-    # HB: essa não é uma inicialização muito boa, melhor que cada item em uma
-    # bin distinta, sem dúvida. Entretanto, uma solução inicial melhor seria
-    # colocar o item na primeira bin das já usadas que ele cabe, não sempre na
-    # última usada ou abrir outra uma nova.
-    
     #first fit
-    '''
+    # HB: o nome da flag me parece invertido, mas sim, esse first fit é
+    # bem melhor do que o que estava sendo usado antes.
     for item in items:
-        new_bin = False
+        old_bin = False
         for bin in current_solution:
             if sum(bin) + item <= bin_capacity:
                 bin.append(item)
-                new_bin = True
+                old_bin = True
                 break
-        if(not new_bin):
+        if(not old_bin):
           current_solution.append([item])
     print("Initial:")
     for bin in current_solution:
         print(bin)
-    '''
 
-    # HB: lembrando que o critério de parada não pode ser tempo. O
-    # critério pode ser um número de iterações. Não confundam esse
-    # critério com o parâmetro h que é o tamanho da lista do LAHC
-    # (e que se refere a quantas iterações atrás você olha para decidir
-    # se vai aceitar ou não a solução).
+    # HB: Por questões de performance é bem melhor guardar na lista só
+    # o valor da solução, porque essa lista só é usada para verificar
+    # tu vais aceitar ou não cada nova solução.
     lh = [current_solution] * 20
     s = lh[0]
     for i in range(1000):
         final_solution = neighbor(s, bin_capacity, s, lh)
         s = final_solution[1]
+        # HB: ??? Essa lista está sendo tratada como uma fila? Porque
+        # aqui parece que vocês sempre só alteram a primeira posição.
+        # A ideia é sempre sobrescrever o valor que está a mais tempo
+        # no array.
         lh[0] = final_solution[2]
 
     return final_solution[0]
@@ -133,7 +118,7 @@ bin_capacity = 10
 #bin, bin_capacity, items = read_input_file('instances/BPP_100_150_0.1_0.7_0.txt')
 
 bin_capacity = 10
-items = [[2],[4],[5],[5]]
+items = [2,4,5,5]
 
 result = bin_packing_lahc(items, bin_capacity)
 
