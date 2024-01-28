014b60c3435c12285333db8f5be0114e05c77e3c
quinhas123 <lucasmschirmbeck@gmail.com>
2024-01-28 20:10:19 -0300

    ajeitar solver

--- solver.py
+++ solver.py
@@ -11,24 +11,24 @@ def read_input_file(fileName):
         return bin, bin_capacity, weights
 
 
-def bin_packing_integer(items, bin_capacity):
+def bin_packing_integer(items, bin_capacity, num_weights):
     # Criação do problema de programação inteira
     prob = LpProblem("BinPackingInteger", LpMinimize)
 
     # Variáveis de decisão binárias
-    x = LpVariable.dicts("item", (range(len(items)), range(len(bin_capacity))), 0, 1, LpInteger)
-    y = LpVariable.dicts("bin", (range(len(bin_capacity))), 0, 1, LpInteger)
+    x = LpVariable.dicts("item", (range(len(items)), range(bin_capacity)), 0, 1, LpInteger)
+    y = LpVariable.dicts("bin", (range(bin_capacity)), 0, 1, LpInteger)
 
     # Função objetivo: Minimizar o número total de contêineres usados
-    prob += lpSum(y[j] for j in range(len(bin_capacity)))
+    prob += lpSum(y[j] for j in range(bin_capacity))
 
     # Restrição: Cada item só pode ser alocado a um contêiner
     for i in range(len(items)):
-        prob += lpSum(x[i][j] for j in range(len(bin_capacity))) == 1
+        prob += lpSum(x[i][j] for j in range(bin_capacity)) == 1
 
     # Restrição: A soma dos tamanhos dos itens em cada contêiner não pode exceder a capacidade
-    for j in range(len(bin_capacity)):
-        prob += lpSum(items[i] * x[i][j] for i in range(len(items))) <= bin_capacity[j]
+    for j in range(bin_capacity):
+        prob += lpSum(items[i] * x[i][j] for i in range(len(items))) <= bin_capacity
 
     """ # Restrição: Xij <= Yj
     for i in range(len(items)):
@@ -38,17 +38,6 @@ def bin_packing_integer(items, bin_capacity):
     # Resolve o problema
     prob.solve()
 
-    # Imprime o status da solução
-    print("Status:", prob.status)
-    # Imprime a solução
-    for j in range(len(bin_capacity)):
-        print(f"Contêiner {j + 1}:")
-        for i in range(len(items)):
-            if x[i][j].value() == 1:
-                print(f"  Item {i + 1} (Tamanho: {items[i]})")
+num_weights, bin_capacity, weights = read_input_file('instances/Falkenauer_t60_00.txt')
 
-# Exemplo de uso
-items = [2, 5, 8, 7, 3]
-bin_capacity = [10, 10]
-
-bin_packing_integer(items, bin_capacity)
+bin_packing_integer(weights, bin_capacity, num_weights)
